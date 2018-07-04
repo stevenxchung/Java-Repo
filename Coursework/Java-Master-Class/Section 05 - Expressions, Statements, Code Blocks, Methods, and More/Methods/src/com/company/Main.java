7b020187162ca7bc3fb24da215ae1efc50c0a53d
Steven <stevenxchung@gmail.com>
2018-07-04 12:54:20 -0400

    Methods initial commit

--- Coursework/Java-Master-Class/Section 05 - Expressions, Statements, Code Blocks, Methods, and More/Methods/src/com/company/Main.java
+++ Coursework/Java-Master-Class/Section 05 - Expressions, Statements, Code Blocks, Methods, and More/Methods/src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int highScore = calculatedScore(true, 800, 5, 10);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculatedScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
     }
+
+    public static int calculatedScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            return finalScore;
+        } else {
+            return -1;
+        }
+    }
+
 }
