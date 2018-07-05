a3b8d4bb6de19ff65dee8c85b687b53897ae91c0
Steven <stevenxchung@gmail.com>
2018-07-05 15:41:04 -0400

    Teen number checker initial commit

---
+++ Coursework/Java-Master-Class/Section 05 - Expressions, Statements, Code Blocks, Methods, and More/Teen Number Checker/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));     // True
+        System.out.println(hasTeen(23, 15, 42));    // True
+        System.out.println(hasTeen(22, 23, 34));    // False
+    }
+
+    public static boolean hasTeen(int first, int second, int third) {
+        if ((first >= 13 && first <= 19) || (second >= 13 && second <= 19) || (third >= 13 && third <= 19)) {
+            return true;
+        }
+        return false;
+    }
+}
