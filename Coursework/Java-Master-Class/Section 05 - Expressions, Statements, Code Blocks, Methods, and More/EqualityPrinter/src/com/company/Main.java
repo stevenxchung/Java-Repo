d18f38890619d7ebac075a4651277b44bbb9438d
Steven <stevenxchung@gmail.com>
2018-07-07 09:10:46 -0400

    Equality printer initial commit

---
+++ Coursework/Java-Master-Class/Section 05 - Expressions, Statements, Code Blocks, Methods, and More/EqualityPrinter/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printEqual(1, 1, 1);
+        printEqual(1, 1, 2);
+        printEqual(-1, -1, -1);
+        printEqual(1, 2, 3);
+    }
+
+    public static void printEqual(int first, int second, int third) {
+        if (first < 0 || second < 0 || third < 0) {
+            System.out.println("Invalid Value");
+        } else if (first == second && second == third && third == first){
+            System.out.println("All numbers are equal");
+        } else if (first != second && second != third && third != first) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
