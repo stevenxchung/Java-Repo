c77a17a9b35b3a3e839a3c343687de330d7b3177
Steven <stevenxchung@gmail.com>
2018-07-07 09:19:59 -0400

    Is cat playing initial commit

---
+++ Coursework/Java-Master-Class/Section 05 - Expressions, Statements, Code Blocks, Methods, and More/PlayingCat/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    isCatPlaying(true, 10);
+	    isCatPlaying(false, 36);
+	    isCatPlaying(false, 35);
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if ((summer && (temperature >= 25 && temperature <= 45)) || (temperature >= 25 && temperature <= 35)) {
+            System.out.println("Cat is playing");
+            return true;
+        }
+        System.out.println("Cat is not playing");
+        return false;
+    }
+}
